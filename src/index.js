8d7bb7f9d40c78846e30e2639c6647dc39edcfd0
tc849948 <student@developer.com>
2026-05-08 17:07:23 +0300

    Add frontend production build (dist) for full stack phonebook

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 const dbPath = path.join(__dirname, '..', 'db.json');
 
